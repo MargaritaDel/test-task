d2867e17c88a468498f5ff814509e79b507d2943
Margaryta Dieltsova <112723193+MargaritaDel@users.noreply.github.com>
2023-06-04 00:48:27 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -10,9 +10,10 @@ import {store } from "./redux/store.js";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}><BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/test-task">
     <App />
-    </BrowserRouter>
+    </BrowserRouter >
     </Provider>
   </React.StrictMode>
 );
