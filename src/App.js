73ed1cfd3e5b61fa4c5f602e58d16e67083c64a3
Margaryta Dieltsova <112723193+MargaritaDel@users.noreply.github.com>
2023-06-04 20:12:17 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import { lazy, Suspense } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
 
-const Home = lazy(() => import('./pages/home/Home'));
-// const NotFound = lazy(() => import('./pages/NotFound'));
-const Tweets = lazy(() => import('./pages/tweets/Tweets'));
-const SharedLayout = lazy(() => import('./components/sharedLayout/SharedLayout'));
+import { Navigate, Route, Routes } from "react-router-dom";
+import SharedLayout from "./components/sharedLayout/SharedLayout";
+import  Home from "./pages/home/Home";
+import  Tweets  from "./pages/tweets/Tweets";
 
 function App() {
   return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<Home />} />
-            <Route path="tweets" element={<Tweets />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </Router>
+    <Routes>
+      <Route path="/" element={<SharedLayout/>}>
+        <Route index element={<Home />} />
+        <Route path="tweets" element={<Tweets />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
   );
 }
+
 export default App;
 
 
